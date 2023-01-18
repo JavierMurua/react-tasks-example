995064bbc0948d089e58154e46110233e6312927
JavierMurua <javierantoniomurua@gmail.com>
2023-01-18 17:33:08 -0300

    Update TaskCard.jsx

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -8,7 +8,7 @@ function TaskCard({ task }) {
     <div className="bg-gray-800 text-white p-4 rounded-md">
       <h1 className="text-xl font-bold capitalize" >{task.title}</h1>
       <p className="text-gray-400" > {task.description} </p>
-      <button className="bg-red-600 px-2 py-1 rounded-md mt-4 hover:bg-red-400" onClick={() => deleteTask(task.id)}>Eliminar tarea</button>
+      <button className="bg-red-600 px-2 py-1 rounded-md mt-4 hover:bg-red-400 rounded-md" onClick={() => deleteTask(task.id)}>Eliminar tarea</button>
     </div>
   );
 }
